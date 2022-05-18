fc9c3c00776b26e26234e3c4668fe89fe7b84987
MathieuHoude <mathieu.houde@codeboxx.biz>
2022-05-18 19:05:57 -0400

    fix getBalance and refactoring

--- blockchain.go
+++ blockchain.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"time"
-)
-
 type Blockchain struct {
 	chain               []*Block
 	pendingTransactions []*Transaction
@@ -12,7 +8,7 @@ type Blockchain struct {
 }
 
 func newBlockchain(_difficulty, _miningReward int) *Blockchain {
-	genesisBlock := &Block{timestamp: time.Now(), previousHash: "", transactions: []*Transaction{}}
+	genesisBlock := newBlock("", _difficulty, []*Transaction{newTransaction("", "myaddress", 100), newTransaction("", "myaddress2", 100)})
 	return &Blockchain{chain: []*Block{genesisBlock}, difficulty: _difficulty, miningReward: _miningReward}
 }
 
@@ -20,19 +16,21 @@ func (b *Blockchain) minePendingTransactions(miningRewardAddress string) {
 	latestBlock := b.chain[len(b.chain)-1]
 	newBlock := newBlock(latestBlock.hash, b.difficulty, b.pendingTransactions)
 	b.chain = append(b.chain, newBlock)
-	b.pendingTransactions = []*Transaction{newTransaction("", "myaddress", b.miningReward)}
+	b.pendingTransactions = []*Transaction{newTransaction("", miningRewardAddress, b.miningReward)}
 }
 
 func (b *Blockchain) getBalance(_address string) int {
 	balance := 0
 
-	for _, transaction := range b.pendingTransactions {
-		if transaction.fromAddress == _address {
-			balance -= transaction.amount
-		}
+	for _, block := range b.chain {
+		for _, transaction := range block.transactions {
+			if transaction.fromAddress == _address {
+				balance -= transaction.amount
+			}
 
-		if transaction.toAddress == _address {
-			balance += transaction.amount
+			if transaction.toAddress == _address {
+				balance += transaction.amount
+			}
 		}
 	}
 
--- main.go
+++ main.go
@@ -6,7 +6,8 @@ func main() {
 	goCoin := newBlockchain(4, 50)
 	goCoin.pendingTransactions = append(goCoin.pendingTransactions, newTransaction("11", "22", 10))
 	goCoin.pendingTransactions = append(goCoin.pendingTransactions, newTransaction("22", "11", 1))
-	goCoin.minePendingTransactions("11")
+	goCoin.minePendingTransactions("myaddress")
+	goCoin.minePendingTransactions("myaddress")
 	balance := goCoin.getBalance("myaddress")
 	fmt.Printf("%d", balance)
 }
